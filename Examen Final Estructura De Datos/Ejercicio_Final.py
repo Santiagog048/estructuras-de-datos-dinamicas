343af1cd4521cf21efdf58c8aeb315ec134f0209
Santiagog048 <126509458+Santiagog048@users.noreply.github.com>
2023-11-17 01:04:37 -0500

    Add files via upload

---
+++ Examen Final Estructura De Datos/Ejercicio_Final.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def in_order_traversal(tree, callback):
+    stack = deque()
+    current = tree
+
+    while stack or current:
+        if current:
+            stack.appendleft(current)
+            current = current.left
+        else:
+            current = stack.popleft()
+            callback(current.value)
+            current = current.right
+
+def someCallback(value):
+    print(f"callback({value})")
+
+tree = BinaryTree(1)
+tree.left = BinaryTree(2)
+tree.right = BinaryTree(3)
+tree.left.left = BinaryTree(4)
+tree.left.left.right = BinaryTree(9)
+tree.right.left = BinaryTree(6)
+tree.right.right = BinaryTree(7)
+
+in_order_traversal(tree, someCallback)
